fb3746d3ed15a53f3690b4be67075479e9e607a2
Darya Grigoryuk <43492784+oast9@users.noreply.github.com>
2020-02-01 02:49:49 +0200

    -

--- Assets/PlaneScan.cs
+++ Assets/PlaneScan.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARKit;
 
 public class PlaneScan : MonoBehaviour
@@ -14,6 +17,14 @@ public class PlaneScan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetMap();
+    }
+
+    private void SetMap()
+    {
+        GameObject session = GameObject.FindGameObjectWithTag("session");
+        ARPlaneManager manager = session.GetComponent<ARPlaneManager>();
+        GameObject map = manager.planePrefab;
+        Debug.Log(map.transform.localScale.x + " " + map.transform.localScale.y + " " + map.transform.localScale.z);
     }
 }
